9227eaa33d0fa2bb576bfea42aafdc4d097c46e1
Siddharth Jadhav <siddharth.jadhav2093@gmail.com>
2018-06-17 15:04:42 +0530

    add signup and home page

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './componets/signup/signup.component';
+import { HomeComponent } from './componets/home/home.component';
+
+const route: Routes = [
+  {path: '', component: SignupComponent},
+  {path: 'home', component: HomeComponent}
+];
+
+@NgModule({
+  exports: [RouterModule],
+  imports: [
+    RouterModule.forRoot(route)
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { SignupComponent } from './componets/signup/signup.component';
+import { HomeComponent } from './componets/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
