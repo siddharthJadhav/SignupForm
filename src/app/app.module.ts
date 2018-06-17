8b88231a8a20ad8881446b48298a10d68403dd1a
Siddharth Jadhav <siddharth.jadhav2093@gmail.com>
2018-06-17 15:45:43 +0530

    add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { SignupComponent } from './componets/signup/signup.component';
 import { HomeComponent } from './componets/home/home.component';
+import { NavbarComponent } from './componets/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
     HomeComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
